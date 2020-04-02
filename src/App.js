9778a566706a27fe1fc9f5b4aea49b22ba81b6ba
Swapnadeep Mohapatra <swapnadeepmohapatra@yahoo.com>
2020-04-02 15:14:53 +0530

    added about me

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/navbar";
 import Header from "./components/header";
+import About from "./components/about";
 
 const App = () => {
   return (
     <div className="App">
       <Navbar />
       <Header />
+      <About />
     </div>
   );
 };
---
+++ src/components/about.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section id="about">
+      <div className="row">
+        <div className="three columns box">
+          <img className="profile-pic" src="images/pp.jfif" alt="" />
+        </div>
+        <div className="nine columns main-col">
+          <h2>About Me</h2>
+          <p>
+            I am 15 years old. Born and brought up in Bhubaneswar and I still
+            live here. I am a self taught photographer. I do Travel photography,
+            Flower photography and Candid photography. Currently diving deeper
+            into the art of editing pictures.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
